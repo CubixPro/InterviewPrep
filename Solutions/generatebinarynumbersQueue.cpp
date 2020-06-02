a06e21623e04979652d0ba3f236089ea28237dce
Titir <titir.adhikary@hotmail.com>
2020-06-02 10:16:45 +0530

    Added code for generating binary numbers(gfg)

---
+++ Solutions/generatebinarynumbersQueue.cpp
@@ -0,0 +1,21 @@
+//generate binary numbers from 1 to n, and every binary number should generated in o(1)
+#include<bits/stdc++.h>
+using namespace std;
+
+void binaryNumbersQueue(int n){
+    queue<string> bin;
+    bin.push("1");
+    while(n != 0){
+        string num = bin.front();
+        bin.pop();
+        cout << num << " ";
+        n--;
+        bin.push(num + "0");
+        bin.push(num + "1");
+    }
+}
+int main(){
+    int size;
+    cin >> size;
+    binaryNumbersQueue(size);
+}
